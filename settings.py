0ee2de470290f9bfe6f34dee3cd793f9b6ad3d51
Steve Pomeroy <steve@staticfree.info>
2012-04-07 01:05:18 -0400

    Emails admins on errors

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # Django settings for barcampboston project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 TIME_ZONE = 'America/New_York'
@@ -51,6 +51,31 @@ INSTALLED_APPS = (
     'taggit',
     'django_gravatar',
 )
+
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
 
 try:
     from local_django_settings import *
